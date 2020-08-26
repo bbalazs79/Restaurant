2424c8583dca685ac5a24954e0bf40ff33d19691
Balog Balázs <b.balazs79@gmail.com>
2020-08-26 19:26:05 +0200

    AuthGuard
    Add roles to AuthGuard

--- restaurant-backend/src/auth/controllers/role.controller.ts
+++ restaurant-backend/src/auth/controllers/role.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, ConflictException, Get, Param, UseGuards } from
 import { RoleDto } from 'dtos/role.dto';
 import { RoleService } from '../services/role.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { Roles } from '../decorators/role.decorator';
 
 @Controller('role')
 export class RoleController {
@@ -20,12 +21,14 @@ export class RoleController {
 
     @Get('')
     @UseGuards(AuthGuard)
+    @Roles('admin')
     async findAll(): Promise<RoleDto> {
         return await this.roleService.findAll();
     }
 
     @Get(':id')
     @UseGuards(AuthGuard)
+    @Roles('admin')
     async findById(@Param('id') id: string): Promise<RoleDto> {
         return await this.roleService.findById(id);
     }
--- restaurant-backend/src/auth/guards/auth.guard.ts
+++ restaurant-backend/src/auth/guards/auth.guard.ts
@@ -1,4 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
 /**
  * Authentikációs guard.
@@ -7,10 +8,16 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
  */
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   public canActivate(
     context: ExecutionContext,
   ): boolean {
     const request = context.switchToHttp().getRequest();
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+    // A roles-ban benne vannak a szükséges role-ok, ellenőrizni kell, hogy a user rendelkezik-e vele.
+
     // Itt szintén boolean-nel kell visszatérni
     // Ha false, akkor az ezzel a Guard-al ellátott végpont 403-al tér vissza.
     return !!request.user;
