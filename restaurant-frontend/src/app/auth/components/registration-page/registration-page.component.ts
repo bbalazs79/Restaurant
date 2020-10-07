20ea0272dcbf307f29c28a2714997148a7ffe07d
Balog Balázs <b.balazs79@gmail.com>
2020-10-07 11:53:17 +0200

    Build registration component, set basic validators

--- restaurant-frontend/src/app/auth/components/registration-page/registration-page.component.ts
+++ restaurant-frontend/src/app/auth/components/registration-page/registration-page.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { NUMBER_TYPE } from "@angular/compiler/src/output/output_ast";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-registration-page',
-  templateUrl: './registration-page.component.html',
-  styleUrls: ['./registration-page.component.scss']
+  selector: "app-registration-page",
+  templateUrl: "./registration-page.component.html",
+  styleUrls: ["./registration-page.component.scss"]
 })
 export class RegistrationPageComponent implements OnInit {
+  authenticationGroup: FormGroup;
+  personalGroup: FormGroup;
+  locationGroup: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.authenticationGroup = this._formBuilder.group({
+      userNameValidator: ["", Validators.required],
+      emailValidator: ["", Validators.required],
+      passwordValidator: ["", Validators.required],
+      passwordAgainValidator: ["", Validators.required]
+    });
+
+    this.personalGroup = this._formBuilder.group({
+      firstNameValidator: ["", Validators.required],
+      lastNameValidator: ["", Validators.required],
+      phoneNumberValidator: ["", Validators.required]
+    });
+
+    this.locationGroup = this._formBuilder.group({
+      zipCodeValidator: ["", Validators.required],
+      cityValidators: ["", Validators.required],
+      streetValidators: ["", Validators.required],
+      houseNumberValidators: ["", Validators.required],
+      storeyValidators: [""],
+      doorNumberValidators: [""],
+      doorBellValidators: [""]
+    });
   }
 
+  registrate() {
+    console.log(this.authenticationGroup);
+  }
 }
