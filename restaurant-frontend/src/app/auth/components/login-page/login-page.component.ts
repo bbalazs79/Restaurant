9f220183ce684d2701ee4519a649b5e70ac2be85
Balog Balázs <b.balazs79@gmail.com>
2020-11-03 10:23:32 +0100

    login correction

--- restaurant-frontend/src/app/auth/components/login-page/login-page.component.ts
+++ restaurant-frontend/src/app/auth/components/login-page/login-page.component.ts
@@ -1,40 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { tap } from 'rxjs/operators';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { tap } from "rxjs/operators";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"]
 })
 export class LoginPageComponent implements OnInit {
-
   public loginForm: FormGroup;
 
   constructor(
     private authService: AuthService,
     //form builder: új formgroup létrehozás
     private fb: FormBuilder,
-  ) { 
+    private router: Router
+  ) {
     //validátorok létrrehozása
     this.loginForm = this.fb.group({
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(3)]],
+      username: ["", [Validators.required]],
+      password: ["", [Validators.required, Validators.minLength(3)]]
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onClickLogin(): void {
-    this.authService.login(this.loginForm.value.username, this.loginForm.value.password)
+    this.authService
+      .login(this.loginForm.value.username, this.loginForm.value.password)
       .pipe(
-        tap((token) => {
-          localStorage.setItem('token', token);
+        tap(token => {
+          localStorage.setItem("token", token);
+          this.router.navigateByUrl("/");
         })
       )
       .subscribe();
   }
-
 }
